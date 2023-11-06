07829b52d99fa38bd6cc322a43649c9d365edaf9
Neo-Uli <124014493+Neo-Uli@users.noreply.github.com>
2023-11-06 16:36:13 -0600

    Update products.py

--- products.py
+++ products.py
@@ -1,5 +1,5 @@
 products = [
-    {"nombre":"laptop","precio":800, "cantidad":4},
-    {"nombre":"mouse","precio":40, "cantidad":10},
-    {"nombre":"monitor","precio":400, "cantidad":3}
+    {"sabor":"laptop","precio":800, "cantidad":4},
+    {"sabor":"mouse","precio":40, "cantidad":10},
+    {"sabor":"monitor","precio":400, "cantidad":3}
 ]
